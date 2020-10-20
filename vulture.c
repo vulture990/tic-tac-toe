4212abbec1e1dd0717a6f7c04458ec2c699789f1
vulture990 <mohamedsali602@live.com>
2020-10-20 17:45:59 +0100

    it fine my 1 rst program works on c that converts the number of minutes to years i may make ffew adjustment to work make it lose some weight when it comes to memory (make it fit lol)

--- vulture.c
+++ vulture.c
@@ -1,7 +1,20 @@
-#include <stdio.h> 
+/*#include <stdio.h> 
 int main(void)
 {
     printf("hello newbie \n");
     printf("learning branches and merging");
+    return 0;
+}
+ so ifigured a  good way of starting of this would be b resolving one of the basics is figuring the convertio of minutes to years*/
+#include <stdio.h>
+int main(void)
+{
+    double minutes;
+    double years ;
+    double x;
+    printf("please enter the number of minutes you want converted: ");
+    scanf("%lf",&minutes);
+    x=(minutes/(525600));
+    printf("easy peezy lemon squezy ,there you go %lf",x);
     return 0;
 }
